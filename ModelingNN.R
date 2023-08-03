221fac8fb48101ccc7fb232e53868a7e26912719
MedinaLab2016 <140093563+MedinaLab2016@users.noreply.github.com>
2023-08-03 12:14:36 -0400

    Update ModelingNN.R

--- ModelingNN.R
+++ ModelingNN.R
@@ -15,27 +15,35 @@ library("neuralnet")
 
 #***
 Comp_data = read_excel("C://Users//Sara Benson//Documents//Masters Project//PeptideMaster.xlsx")
+
+#Removing any duplicated data  
 Comp_data <- Comp_data[!duplicated(Comp_data), ]
 require(caTools)
 
+#Calling all properties of peptides other than sequences
 Comp_dataMIC1 = Comp_data[,2:15]
 
 dft = Comp_dataMIC1
+
+#Putting MIC values in MIC^-1  
 dft$MIC1 = (1/dft$MIC1)
+
+#Generating train and test sets
 sample_size = round(nrow(dft)*1) 
 index <- sample(seq_len(nrow(dft)), size = sample_size)
 
 train <- dft[index, ]
 test <- dft[-index, ]
 
+#Neural network function
 NN <- neuralnet(
   MIC1 ~ .,
   data=data.frame(train),
-  hidden=10,
-  rep=4,
-  hreshold=0.01,
-  err.fct="sse",
-  linear.output=FALSE
+  hidden=10, #number of hidden neurons (vertices) in each layer
+  rep=4, #the number of repetitions for the neural network's training
+  threshold=0.01, #numeric value specifying the threshold for the partial derivatives of the error function as stopping criteria
+  err.fct="sse", #error function used, sum of squared errors
+  linear.output=FALSE #logical, ff act.fct should not be applied to the output neurons set linear output to TRUE, otherwise to FALSE
 )
 
 #############################################################################################
@@ -48,20 +56,26 @@ fname7 = paste0("C://Users//Sara Benson//Documents//Masters Project//Grant//Auto
 newdata = read_excel(fname7)
 #***
 newdata = read_excel("C://Users//Sara Benson//Documents//Masters Project//Grant//Autotest//Gen1automated.xlsx")
+
+#Creating dataframe of new sequences
 gen.S = data.frame(newdata$Sequence)
 
+#New biophysical property data  
 newdata = newdata[,2:14]
 
-predNNw = predict(NN, newdata=data.frame(newdata))
+#Predicting new values   
+predNN = predict(NN, newdata=data.frame(newdata))
 
 gen.M = data.frame(predNNnew)
 library(writexl)
 
-choose = 10 #choose the top 10 
+ #Choosing top 10 sequences  
+choose = 10 
 gen.total = data.frame(c(gen.S,gen.M))
 gen.total = gen.total[order(-gen.total$predNNnew),]
 gen.total = gen.total[1:choose,]
 
+#Creating new excel file  
 #***
 fname8 = paste0("C://Users//Sara Benson//Documents//Masters Project//Grant//Autotest//Gen",gen+1, "data.xlsx")
 write.xlsx(gen.total, fname8)
